6c03ce3e3344297c11383ff4b1bb36339a096ce2
GavinKZ <144485700+GavinKZ@users.noreply.github.com>
2023-09-12 17:33:53 -0300

    Exercício 4

--- Lista_de_exercícios_1.sql
+++ Lista_de_exercícios_1.sql
@@ -9,3 +9,8 @@ where nascimento <= "1900-01-01";
 select titulo from livros l
 join autores a on a.id = l.autor_id
 where a.nome = "J.K. Rowling";
+
+-- 4.
+select aluno_id, nome from alunos a
+left join matriculas m on m.id = a.id
+where curso = "Engenharia de Software"
