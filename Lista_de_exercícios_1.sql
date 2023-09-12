95e6371317bbd24caefb1f49ba16e157da7c2d5b
GavinKZ <144485700+GavinKZ@users.noreply.github.com>
2023-09-12 17:31:21 -0300

    Exercício 3

--- Lista_de_exercícios_1.sql
+++ Lista_de_exercícios_1.sql
@@ -4,3 +4,8 @@ select titulo from livros;
 -- 2.
 select nome from autores
 where nascimento <= "1900-01-01";
+
+-- 3.
+select titulo from livros l
+join autores a on a.id = l.autor_id
+where a.nome = "J.K. Rowling";
